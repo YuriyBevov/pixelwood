fd9b848c78173b449bf403db9a6a2318e40045ed
Yuriy Bevov <jury19y@mail.ru>
2022-05-21 16:11:59 +0300

    modal width

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,7 @@
-//import './modules/pageAnimation.js';
+import './modules/pageAnimation.js';
 import './modules/stickyHeader.js';
 import './modules/iMask.js';
 import './modules/accordeon.js';
 import './modules/modal.js';
 import './modules/swiper.js';
 import './modules/form.js';
-
